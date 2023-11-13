4957ce4743bbeaf87e0ba7039139ee8af8703cb5
AnnPey <peycheva440@gmail.ua>
2023-11-13 20:50:28 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+
+  listItem.textContent = ingredient;
+
+  listItem.classList.add("item");
+
+  ingredientsList.appendChild(listItem);
+});
+
+console.log(ingredientsList);
