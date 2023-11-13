35480d61f46d61ee6a1541cb498b4bd41d059090
AnnPey <peycheva440@gmail.ua>
2023-11-13 20:54:06 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = this.elements.email.value;
+  const password = this.elements.password.value;
+  const credentials = {
+    email: email,
+    password: password,
+  };
+
+  if (email.trim() === "" || password.trim() === "") {
+    alert("Please fill all the fields");
+  // } else if (email !== "example@example.com" || password !== "password") {
+  //   alert("Incorrect data");
+  } else {
+    alert("Success");
+    console.log(credentials);
+    
+  }
+});
 
